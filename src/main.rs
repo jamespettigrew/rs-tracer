cb9fa5ce30384ee98c2e7f9275a9f9730f6de158
Ryan Carrier <ryan.j.carrier@gmail.com>
2017-11-26 13:37:00 +1000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ extern crate cgmath;
 extern crate image as im;
 extern crate piston_window;
 
-use cgmath::{ InnerSpace, Point3, Vector3 };
-use im::{ Rgba, RgbaImage };
+use cgmath::{InnerSpace, Point3, Vector3};
+use im::{Rgba, RgbaImage};
 use piston_window::*;
 
 struct Sphere {
     center: Point3<f32>,
-    radius: f32
+    radius: f32,
 }
 
 impl Sphere {
@@ -45,57 +45,58 @@ impl Sphere {
 
 struct Ray {
     origin: Point3<f32>,
-    direction: Vector3<f32>
+    direction: Vector3<f32>,
 }
 
 struct Camera {
     position: Point3<f32>,
     up: Vector3<f32>,
     at: Vector3<f32>,
-    fov: f32
+    fov: f32,
 }
 
 struct RenderOptions {
     width: u32,
-    height: u32
+    height: u32,
 }
 
 struct Scene {
-    spheres: Vec<Sphere>
+    spheres: Vec<Sphere>,
 }
 
 fn closest_intersection<'a>(scene: &'a Scene, ray: &Ray) -> Option<(&'a Sphere, f32)> {
-    scene.spheres
-         .as_slice()
-         .into_iter()
-         .filter_map(|s| { 
-             let intersection = s.intersects(ray);
-             match intersection {
-                 Some(i) => {
-                     return if i.is_nan() { None } else { Some((s, i)) };
-                 },
-                 None => None
-             }
-         })
-         .min_by(|x, y| { 
-             let &(s1, i1) = x;
-             let &(s2, i2) = y;
-             return i1.partial_cmp(&i2).unwrap(); // Shouldn't ever hit NaN due to check above
-         })
+    scene
+        .spheres
+        .as_slice()
+        .into_iter()
+        .filter_map(|s| {
+            let intersection = s.intersects(ray);
+            match intersection {
+                Some(i) => {
+                    return if i.is_nan() { None } else { Some((s, i)) };
+                }
+                None => None,
+            }
+        })
+        .min_by(|x, y| {
+            let &(s1, i1) = x;
+            let &(s2, i2) = y;
+            return i1.partial_cmp(&i2).unwrap(); // Shouldn't ever hit NaN due to check above
+        })
 }
 
 fn get_pixel_color(scene: &Scene, ray: &Ray) -> Rgba<u8> {
     let closest_intersection = closest_intersection(&scene, ray);
     match closest_intersection {
-        Some(i) => { 
+        Some(i) => {
             let (sphere, ray_distance) = i;
             let intersection_point = ray.origin + (ray.direction * ray_distance);
             let normal = sphere.normal(intersection_point);
             let facing_ratio = 0f32.max(normal.dot(-ray.direction));
             let shade: u8 = (255.0 * facing_ratio) as u8;
             return Rgba([shade, shade, shade, 255]);
-        },
-        None => Rgba([0, 0, 0, 255])
+        }
+        None => Rgba([0, 0, 0, 255]),
     }
 }
 
@@ -126,7 +127,10 @@ fn render_frame(scene: &Scene, camera: &Camera, render_options: &RenderOptions) 
             let px_camera_space = Point3::new(px_screen_x, px_screen_y, -1.0);
 
             let ray_vector = (px_camera_space - camera.position).normalize();
-            let ray = Ray { origin: camera.position, direction: ray_vector };
+            let ray = Ray {
+                origin: camera.position,
+                direction: ray_vector,
+            };
 
             let color = get_pixel_color(scene, &ray);
             img.put_pixel(px_x, px_y, color);
@@ -138,18 +142,47 @@ fn render_frame(scene: &Scene, camera: &Camera, render_options: &RenderOptions) 
 
 fn main() {
     let mut spheres = Vec::new();
-    spheres.push(Sphere { center: Point3 { x: -2.0, y: 0.0, z: -4.0 }, radius: 1.0 });
-    spheres.push(Sphere { center: Point3 { x: 4.0, y: 2.0, z: -10.0 }, radius: 0.9 });
+    spheres.push(Sphere {
+        center: Point3 {
+            x: -2.0,
+            y: 0.0,
+            z: -4.0,
+        },
+        radius: 1.0,
+    });
+    spheres.push(Sphere {
+        center: Point3 {
+            x: 4.0,
+            y: 2.0,
+            z: -10.0,
+        },
+        radius: 0.9,
+    });
 
     let mut scene = Scene { spheres: spheres };
 
     let camera = Camera {
-        position: Point3 { x: 0.0, y: 0.0, z: 0.0 },
-        up: Vector3  { x: 0.0, y: 1.0, z: 0.0 },
-        at: Vector3 { x: 1.0, y: 0.0, z: 0.0 },
-        fov: 90.0
+        position: Point3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        up: Vector3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        at: Vector3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        fov: 90.0,
     };
-    let render_options = RenderOptions { width: 640, height: 640 };
+    let render_options = RenderOptions {
+        width: 640,
+        height: 640,
+    };
 
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow =
@@ -164,11 +197,8 @@ fn main() {
     while let Some(e) = window.next() {
         let frame = render_frame(&scene, &camera, &render_options);
 
-        let texture: G2dTexture = Texture::from_image(
-            &mut window.factory,
-            &frame,
-            &TextureSettings::new()
-        ).unwrap();
+        let texture: G2dTexture =
+            Texture::from_image(&mut window.factory, &frame, &TextureSettings::new()).unwrap();
 
         window.draw_2d(&e, |c, g| {
             clear([1.0; 4], g);
